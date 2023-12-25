eeb2b5c56047a1bec83412766b4ecdbfccf88eb1
Impa10r <impalor@pm.me>
2023-12-25 10:41:57 +0100

    This has a bug that all api calls return empty values

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # PeerSwap Web UI
+Server-side rendered UI to connect to PeerSwap LND daemon. 
 
-Simple server-side rendered web site to connect to your PeerSwap instance 
+# Instalation
 
-Uses mempool.space API
+# Development
+You will need to create an `.env` file on the root dir. An easy way of doing this is just to copy the sample file to `.env`.
 
+```
+cp .env-sample .env
+```
+
+REST API client was generated with https://github.com/go-swagger/go-swagger using https://github.com/ElementsProject/peerswap/blob/master/peerswaprpc/peerswaprpc.swagger.json
+
+```
+.\swagger generate client -f peerswaprpc.swagger.json -A peerswap-rest-cli
+```
+dlv debug ~/go/src/peerswap-web/cmd/web/main.go --headless --listen=:3000 --log
---
+++ cmd/web/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-openapi/strfmt"
+
+	apiclient "peerswap-web/client"
+
+	httptransport "github.com/go-openapi/runtime/client"
+)
+
+type Page struct {
+	Title     string
+	SatAmount string
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", indexPage)
+
+	log.Println("Listening on http://localhost:8088")
+	err := http.ListenAndServe(":8088", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+
+	RpcHost := os.Getenv("RPC_HOST")
+
+	if RpcHost == "" {
+		RpcHost = "localhost:42070"
+	}
+	// create the transport
+	transport := httptransport.New(RpcHost, "", nil)
+
+	// create the API client, with the transport
+	client := apiclient.New(transport, strfmt.Default)
+
+	// make the request to get liquid balance
+	resp, err := client.PeerSwap.PeerSwapLiquidGetBalance(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	satAmount := resp.Payload.SatAmount
+
+	fmt.Printf("%#v\n", resp.Payload)
+
+	tmpl, err := template.ParseFiles("layout.html")
+	if err != nil {
+		// Log the detailed error
+		log.Println(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	data := Page{
+		Title:     "PeerSwap",
+		SatAmount: satAmount,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Fatalln(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
+}
