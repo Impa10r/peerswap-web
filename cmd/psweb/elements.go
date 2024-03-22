61398bd77bea8374fab94db84427da3f7c5e4941
Impa10r <impalor@pm.me>
2024-03-22 19:44:57 +0100

    claimpegin timeout 5 minutes

--- cmd/psweb/elements.go
+++ cmd/psweb/elements.go
@@ -150,7 +150,7 @@ func ElementsClient() (c *RPCClient) {
 		user:       user,
 		passwd:     passwd,
 		httpClient: httpClient,
-		timeout:    600} // 10 minutes for peg-in
+		timeout:    300} // 5 minutes for claimpegin
 	return
 }
 
